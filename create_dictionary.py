223a52fa3deb08dac64d75b0cd5d0c627751dfae
kviligit <56066509+kviligit@users.noreply.github.com>
2021-12-19 19:40:26 +0100

    create program file: detailed_list_to_wordlist.py. The intetion is to create a programme that converts the list given, into a list only containing words

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,3 +1,4 @@
 print("Hello! And congratulations on stating a new keeb-adventure!")
 letters = input("Enter the letters you want to practice, without any separation.\n")
+
 print ("You would like to practice the following letters:" + letters )

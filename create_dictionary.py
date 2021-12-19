8d0a383afa3e1c8b0451c401e7fff797ee25b517
kviligit <56066509+kviligit@users.noreply.github.com>
2021-12-19 19:10:05 +0100

    welcoming message and input request ready

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,1 +1,3 @@
-
+print("Hello! And congratulations on stating a new keeb-adventure!")
+letters = input("Enter the letters you want to practice, without any separation.\n")
+print ("You would like to practice the following letters:" + letters )
